42ce8c955179469a03d2fb0bdb62e70f14bc3ab9
Alessio La Greca <alessio.lagreca13@gmail.com>
2022-02-23 22:23:30 +0100

    Added generation of compartments

--- Assets/InputManagerScript.cs
+++ Assets/InputManagerScript.cs
@@ -17,6 +17,8 @@ public class InputManagerScript : MonoBehaviour
 
     public GameObject UIToHide;
 
+    public GameObject LineGO;
+
     public void getInputBox()
     {
         bool success = Int32.TryParse(xInput.text, out int x);
@@ -35,7 +37,80 @@ public class InputManagerScript : MonoBehaviour
 
     public void getInputCompartment()
     {
+        bool success = Int32.TryParse(xInput.text, out int x);
+        success &= Int32.TryParse(yInput.text, out int y);
+        success &= Int32.TryParse(zInput.text, out int z);
 
+        if (success)
+        {
+            GameObject[] lines = new GameObject[12];
+            GameObject[] tmp = generatePlaneOnHeight(0, x, z);
+            for(int i = 0; i < 4; i++)
+            {
+                lines[i] = tmp[i];
+            }
+            tmp = generatePlaneOnHeight(y, x, z);
+            for (int i = 0; i < 4; i++)
+            {
+                lines[i+4] = tmp[i];
+            }
+
+            lines[8] = Instantiate(LineGO);
+            LineRenderer lr = lines[8].GetComponent<LineRenderer>();
+            lr.SetPosition(0, new Vector3(0, 0, 0));
+            lr.SetPosition(1, new Vector3(0, y, 0));
+
+            lines[9] = Instantiate(LineGO);
+            lr = lines[9].GetComponent<LineRenderer>();
+            lr.SetPosition(0, new Vector3(x, 0, 0));
+            lr.SetPosition(1, new Vector3(x, y, 0));
+
+            lines[10] = Instantiate(LineGO);
+            lr = lines[10].GetComponent<LineRenderer>();
+            lr.SetPosition(0, new Vector3(0, 0, z));
+            lr.SetPosition(1, new Vector3(0, y, z));
+
+            lines[11] = Instantiate(LineGO);
+            lr = lines[11].GetComponent<LineRenderer>();
+            lr.SetPosition(0, new Vector3(x, 0, z));
+            lr.SetPosition(1, new Vector3(x, y, z));
+
+        }
+
+
+
+
+
+
+        xInput.text = "";
+        yInput.text = "";
+        zInput.text = "";
+    }
+
+    private GameObject[] generatePlaneOnHeight(int y, int x, int z)
+    {
+        GameObject[] lines = new GameObject[4];
+        lines[0] = Instantiate(LineGO);
+        LineRenderer lr = lines[0].GetComponent<LineRenderer>();
+        lr.SetPosition(0, new Vector3(0, y, 0));
+        lr.SetPosition(1, new Vector3(x, y, 0));
+
+        lines[1] = Instantiate(LineGO);
+        lr = lines[1].GetComponent<LineRenderer>();
+        lr.SetPosition(0, new Vector3(0, y, 0));
+        lr.SetPosition(1, new Vector3(0, y, z));
+
+        lines[2] = Instantiate(LineGO);
+        lr = lines[2].GetComponent<LineRenderer>();
+        lr.SetPosition(0, new Vector3(x, y, z));
+        lr.SetPosition(1, new Vector3(x, y, 0));
+
+        lines[3] = Instantiate(LineGO);
+        lr = lines[3].GetComponent<LineRenderer>();
+        lr.SetPosition(0, new Vector3(x, y, z));
+        lr.SetPosition(1, new Vector3(0, y, z));
+
+        return lines;
     }
 
     public void setUI()
